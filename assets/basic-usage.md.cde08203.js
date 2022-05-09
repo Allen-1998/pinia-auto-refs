9ff90d9f1afbac422514d67f1d2f023061bbfd1c
Allen-1998 <73213399+Allen-1998@users.noreply.github.com>
2022-05-09 19:19:46 +0000

    Deploying to gh-pages from @ Allen-1998/pinia-auto-refs@901a1bb52a4dec7e8d9362bb7d06c7daa3e60585 🚀

---
+++ assets/basic-usage.md.cde08203.js
@@ -0,0 +1,62 @@
+import{_ as s,c as n,o as a,a as t}from"./app.f159318c.js";const g='{"title":"Basic usage","description":"","frontmatter":{},"headers":[{"level":3,"title":"store/index.ts","slug":"store-index-ts"},{"level":3,"title":"store/test.ts","slug":"store-test-ts"},{"level":3,"title":"store/user.ts","slug":"store-user-ts"},{"level":3,"title":"helper/pinia-auto-refs.ts","slug":"helper-pinia-auto-refs-ts"},{"level":3,"title":"useStore Demo","slug":"usestore-demo"}],"relativePath":"basic-usage.md"}',p={},o=t(`<h1 id="basic-usage" tabindex="-1">Basic usage <a class="header-anchor" href="#basic-usage" aria-hidden="true">#</a></h1><h3 id="store-index-ts" tabindex="-1">store/index.ts <a class="header-anchor" href="#store-index-ts" aria-hidden="true">#</a></h3><div class="language-ts"><pre><code><span class="token keyword">const</span> store <span class="token operator">=</span> <span class="token function">createPinia</span><span class="token punctuation">(</span><span class="token punctuation">)</span>
+
+<span class="token keyword">export</span> <span class="token keyword">default</span> store
+</code></pre></div><h3 id="store-test-ts" tabindex="-1">store/test.ts <a class="header-anchor" href="#store-test-ts" aria-hidden="true">#</a></h3><div class="language-ts"><pre><code><span class="token keyword">export</span> <span class="token keyword">default</span> <span class="token function">defineStore</span><span class="token punctuation">(</span><span class="token punctuation">{</span>
+  id<span class="token operator">:</span> <span class="token string">&#39;test&#39;</span><span class="token punctuation">,</span>
+  <span class="token function-variable function">state</span><span class="token operator">:</span> <span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token operator">=&gt;</span> <span class="token punctuation">{</span>
+    <span class="token keyword">return</span> <span class="token punctuation">{</span>
+      name<span class="token operator">:</span> <span class="token string">&#39;allen&#39;</span><span class="token punctuation">,</span>
+      token<span class="token operator">:</span> <span class="token string">&#39;token...&#39;</span><span class="token punctuation">,</span>
+    <span class="token punctuation">}</span>
+  <span class="token punctuation">}</span><span class="token punctuation">,</span>
+  getters<span class="token operator">:</span> <span class="token punctuation">{</span>
+    <span class="token function-variable function">fullName</span><span class="token operator">:</span> <span class="token punctuation">(</span><span class="token punctuation">{</span> name <span class="token punctuation">}</span><span class="token punctuation">)</span> <span class="token operator">=&gt;</span> <span class="token punctuation">{</span>
+      <span class="token keyword">return</span> name <span class="token operator">+</span> <span class="token string">&#39;_ttk&#39;</span>
+    <span class="token punctuation">}</span><span class="token punctuation">,</span>
+  <span class="token punctuation">}</span><span class="token punctuation">,</span>
+  actions<span class="token operator">:</span> <span class="token punctuation">{</span>
+    <span class="token function">updateName</span><span class="token punctuation">(</span>name<span class="token operator">:</span> <span class="token builtin">string</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+      <span class="token keyword">this</span><span class="token punctuation">.</span>name <span class="token operator">=</span> name
+    <span class="token punctuation">}</span><span class="token punctuation">,</span>
+  <span class="token punctuation">}</span><span class="token punctuation">,</span>
+<span class="token punctuation">}</span><span class="token punctuation">)</span>
+</code></pre></div><h3 id="store-user-ts" tabindex="-1">store/user.ts <a class="header-anchor" href="#store-user-ts" aria-hidden="true">#</a></h3><div class="language-ts"><pre><code><span class="token keyword">export</span> <span class="token keyword">default</span> <span class="token function">defineStore</span><span class="token punctuation">(</span><span class="token punctuation">{</span>
+  id<span class="token operator">:</span> <span class="token string">&#39;user&#39;</span><span class="token punctuation">,</span>
+  <span class="token function-variable function">state</span><span class="token operator">:</span> <span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token operator">=&gt;</span> <span class="token punctuation">{</span>
+    <span class="token keyword">return</span> <span class="token punctuation">{</span>
+      userInfo<span class="token operator">:</span> <span class="token punctuation">{</span>
+        name<span class="token operator">:</span> <span class="token string">&#39;&#39;</span><span class="token punctuation">,</span>
+        token<span class="token operator">:</span> <span class="token string">&#39;&#39;</span><span class="token punctuation">,</span>
+      <span class="token punctuation">}</span><span class="token punctuation">,</span>
+    <span class="token punctuation">}</span>
+  <span class="token punctuation">}</span><span class="token punctuation">,</span>
+<span class="token punctuation">}</span><span class="token punctuation">)</span>
+</code></pre></div><h3 id="helper-pinia-auto-refs-ts" tabindex="-1">helper/pinia-auto-refs.ts <a class="header-anchor" href="#helper-pinia-auto-refs-ts" aria-hidden="true">#</a></h3><div class="language-ts"><pre><code><span class="token comment">// &quot;https://github.com/Allen-1998/pinia-auto-refs&quot;</span>
+<span class="token keyword">import</span> testStore <span class="token keyword">from</span> <span class="token string">&#39;@/store/test&#39;</span>
+<span class="token keyword">import</span> userStore <span class="token keyword">from</span> <span class="token string">&#39;@/store/user&#39;</span>
+
+<span class="token keyword">import</span> <span class="token punctuation">{</span> ToRef<span class="token punctuation">,</span> StoreToRefs <span class="token punctuation">}</span> <span class="token keyword">from</span> <span class="token string">&#39;vue&#39;</span>
+<span class="token keyword">declare</span> <span class="token keyword">module</span> <span class="token string">&#39;vue&#39;</span> <span class="token punctuation">{</span>
+  <span class="token keyword">export</span> <span class="token keyword">type</span> <span class="token class-name">StoreToRefs<span class="token operator">&lt;</span><span class="token constant">T</span><span class="token operator">&gt;</span></span> <span class="token operator">=</span> <span class="token punctuation">{</span>
+    <span class="token punctuation">[</span><span class="token constant">K</span> <span class="token keyword">in</span> <span class="token keyword">keyof</span> <span class="token constant">T</span><span class="token punctuation">]</span><span class="token operator">:</span> <span class="token constant">T</span><span class="token punctuation">[</span><span class="token constant">K</span><span class="token punctuation">]</span> <span class="token keyword">extends</span> <span class="token class-name"><span class="token builtin">Function</span></span> <span class="token operator">?</span> <span class="token constant">T</span><span class="token punctuation">[</span><span class="token constant">K</span><span class="token punctuation">]</span> <span class="token operator">:</span> ToRef<span class="token operator">&lt;</span><span class="token constant">T</span><span class="token punctuation">[</span><span class="token constant">K</span><span class="token punctuation">]</span><span class="token operator">&gt;</span>
+  <span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+<span class="token keyword">declare</span> <span class="token keyword">type</span> <span class="token class-name">PickOne<span class="token operator">&lt;</span><span class="token constant">T</span><span class="token punctuation">,</span> <span class="token constant">K</span> <span class="token keyword">extends</span> <span class="token keyword">keyof</span> <span class="token constant">T</span><span class="token operator">&gt;</span></span> <span class="token operator">=</span> <span class="token constant">T</span><span class="token punctuation">[</span><span class="token constant">K</span><span class="token punctuation">]</span>
+
+<span class="token keyword">const</span> storeExports <span class="token operator">=</span> <span class="token punctuation">{</span>
+  test<span class="token operator">:</span> testStore<span class="token punctuation">,</span>
+  user<span class="token operator">:</span> userStore<span class="token punctuation">,</span>
+<span class="token punctuation">}</span>
+
+<span class="token keyword">export</span> <span class="token keyword">function</span> <span class="token generic-function"><span class="token function">useStore</span><span class="token generic class-name"><span class="token operator">&lt;</span><span class="token constant">T</span> <span class="token keyword">extends</span> <span class="token keyword">keyof</span> <span class="token keyword">typeof</span> storeExports<span class="token operator">&gt;</span></span></span><span class="token punctuation">(</span>storeName<span class="token operator">:</span> <span class="token constant">T</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+  <span class="token keyword">const</span> store <span class="token operator">=</span> storeExports<span class="token punctuation">[</span>storeName<span class="token punctuation">]</span><span class="token punctuation">(</span><span class="token punctuation">)</span>
+  <span class="token keyword">const</span> storeRefs <span class="token operator">=</span> <span class="token function">storeToRefs</span><span class="token punctuation">(</span>store<span class="token punctuation">)</span>
+  <span class="token keyword">return</span> <span class="token punctuation">{</span> <span class="token operator">...</span>store<span class="token punctuation">,</span> <span class="token operator">...</span>storeRefs <span class="token punctuation">}</span> <span class="token keyword">as</span> <span class="token builtin">unknown</span> <span class="token keyword">as</span> StoreToRefs<span class="token operator">&lt;</span>
+    ReturnType<span class="token operator">&lt;</span>PickOne<span class="token operator">&lt;</span><span class="token keyword">typeof</span> storeExports<span class="token punctuation">,</span> <span class="token constant">T</span><span class="token operator">&gt;&gt;</span>
+  <span class="token operator">&gt;</span>
+<span class="token punctuation">}</span>
+</code></pre></div><h3 id="usestore-demo" tabindex="-1">useStore Demo <a class="header-anchor" href="#usestore-demo" aria-hidden="true">#</a></h3><div class="language-ts"><pre><code><span class="token keyword">const</span> <span class="token punctuation">{</span> name<span class="token punctuation">,</span> token<span class="token punctuation">,</span> fullName<span class="token punctuation">,</span> updateName <span class="token punctuation">}</span> <span class="token operator">=</span> <span class="token function">useStore</span><span class="token punctuation">(</span><span class="token string">&#39;test&#39;</span><span class="token punctuation">)</span>
+
+<span class="token comment">// name.value</span>
+<span class="token comment">// updateName()</span>
+</code></pre></div>`,11),e=[o];function c(l,r,k,u,i,d){return a(),n("div",null,e)}var h=s(p,[["render",c]]);export{g as __pageData,h as default};
---
+++ assets/index.md.d548bb5d.js
@@ -0,0 +1,50 @@
+import{_ as n,c as s,o as a,a as t}from"./app.f159318c.js";const g='{"title":"Getting started","description":"","frontmatter":{},"headers":[{"level":2,"title":"without","slug":"without"},{"level":2,"title":"with","slug":"with"},{"level":2,"title":"Install","slug":"install"},{"level":2,"title":"Setup","slug":"setup"},{"level":2,"title":"Typescript definitions","slug":"typescript-definitions"},{"level":2,"title":"Config Options","slug":"config-options"}],"relativePath":"index.md"}',p={},o=t(`<h1 id="getting-started" tabindex="-1">Getting started <a class="header-anchor" href="#getting-started" aria-hidden="true">#</a></h1><h2 id="without" tabindex="-1">without <a class="header-anchor" href="#without" aria-hidden="true">#</a></h2><div class="language-ts"><pre><code><span class="token keyword">import</span> useUserStore <span class="token keyword">from</span> <span class="token string">&#39;@/store/user&#39;</span>
+
+<span class="token keyword">const</span> userStore <span class="token operator">=</span> <span class="token function">useUserStore</span><span class="token punctuation">(</span><span class="token punctuation">)</span>
+<span class="token keyword">const</span> <span class="token punctuation">{</span> name<span class="token punctuation">,</span> token<span class="token punctuation">,</span> fullName <span class="token punctuation">}</span> <span class="token operator">=</span> <span class="token function">storeToRefs</span><span class="token punctuation">(</span>userStore<span class="token punctuation">)</span>
+<span class="token keyword">const</span> <span class="token punctuation">{</span> updateName <span class="token punctuation">}</span> <span class="token operator">=</span> userStore
+</code></pre></div><h2 id="with" tabindex="-1">with <a class="header-anchor" href="#with" aria-hidden="true">#</a></h2><div class="language-ts"><pre><code><span class="token keyword">const</span> <span class="token punctuation">{</span> name<span class="token punctuation">,</span> token<span class="token punctuation">,</span> fullName<span class="token punctuation">,</span> updateName <span class="token punctuation">}</span> <span class="token operator">=</span> <span class="token function">useStore</span><span class="token punctuation">(</span><span class="token string">&#39;user&#39;</span><span class="token punctuation">)</span>
+</code></pre></div><h2 id="install" tabindex="-1">Install <a class="header-anchor" href="#install" aria-hidden="true">#</a></h2><p><code>npm i pinia-auto-refs</code></p><h2 id="setup" tabindex="-1">Setup <a class="header-anchor" href="#setup" aria-hidden="true">#</a></h2><div class="language-ts"><pre><code><span class="token comment">// vite.config.ts</span>
+<span class="token keyword">import</span> <span class="token punctuation">{</span> defineConfig <span class="token punctuation">}</span> <span class="token keyword">from</span> <span class="token string">&#39;vite&#39;</span>
+<span class="token keyword">import</span> AutoImport <span class="token keyword">from</span> <span class="token string">&#39;unplugin-auto-import/vite&#39;</span>
+<span class="token keyword">import</span> PiniaAutoRefs <span class="token keyword">from</span> <span class="token string">&#39;pinia-auto-refs&#39;</span>
+
+<span class="token keyword">export</span> <span class="token keyword">default</span> <span class="token function">defineConfig</span><span class="token punctuation">(</span><span class="token punctuation">{</span>
+  resolve<span class="token operator">:</span> <span class="token punctuation">{</span>
+    alias<span class="token operator">:</span> <span class="token punctuation">{</span>
+      <span class="token string-property property">&#39;@&#39;</span><span class="token operator">:</span> <span class="token function">resolve</span><span class="token punctuation">(</span>__dirname<span class="token punctuation">,</span> <span class="token string">&#39;src&#39;</span><span class="token punctuation">)</span><span class="token punctuation">,</span>
+    <span class="token punctuation">}</span><span class="token punctuation">,</span>
+  <span class="token punctuation">}</span><span class="token punctuation">,</span>
+  plugins<span class="token operator">:</span> <span class="token punctuation">[</span>
+    <span class="token function">AutoImport</span><span class="token punctuation">(</span><span class="token punctuation">{</span>
+      dts<span class="token operator">:</span> <span class="token string">&#39;src/auto-imports.d.ts&#39;</span><span class="token punctuation">,</span>
+      imports<span class="token operator">:</span> <span class="token punctuation">[</span>
+        <span class="token string">&#39;vue&#39;</span><span class="token punctuation">,</span>
+        <span class="token string">&#39;vue-router&#39;</span><span class="token punctuation">,</span>
+        <span class="token string">&#39;pinia&#39;</span><span class="token punctuation">,</span>
+        <span class="token punctuation">{</span>
+          <span class="token string-property property">&#39;@/helper/pinia-auto-refs&#39;</span><span class="token operator">:</span> <span class="token punctuation">[</span><span class="token string">&#39;useStore&#39;</span><span class="token punctuation">]</span><span class="token punctuation">,</span> <span class="token comment">// !important</span>
+        <span class="token punctuation">}</span><span class="token punctuation">,</span>
+      <span class="token punctuation">]</span><span class="token punctuation">,</span>
+      resolvers<span class="token operator">:</span> <span class="token punctuation">[</span><span class="token function">ElementPlusResolver</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">]</span><span class="token punctuation">,</span>
+    <span class="token punctuation">}</span><span class="token punctuation">)</span><span class="token punctuation">,</span>
+    <span class="token function">PiniaAutoRefs</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">,</span>
+  <span class="token punctuation">]</span><span class="token punctuation">,</span>
+<span class="token punctuation">}</span><span class="token punctuation">)</span>
+</code></pre></div><h2 id="typescript-definitions" tabindex="-1">Typescript definitions <a class="header-anchor" href="#typescript-definitions" aria-hidden="true">#</a></h2><div class="language-json"><pre><code><span class="token punctuation">{</span>
+  <span class="token property">&quot;compilerOptions&quot;</span><span class="token operator">:</span> <span class="token punctuation">{</span>
+    <span class="token property">&quot;types&quot;</span><span class="token operator">:</span> <span class="token punctuation">[</span><span class="token string">&quot;pinia-auto-refs&quot;</span><span class="token punctuation">]</span>
+  <span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</code></pre></div><h2 id="config-options" tabindex="-1">Config Options <a class="header-anchor" href="#config-options" aria-hidden="true">#</a></h2><div class="language-ts"><pre><code><span class="token keyword">type</span> <span class="token class-name">Options</span> <span class="token operator">=</span> Partial<span class="token operator">&lt;</span><span class="token punctuation">{</span>
+  storeDir<span class="token operator">:</span> <span class="token builtin">string</span>
+  excludes<span class="token operator">:</span> <span class="token builtin">string</span><span class="token punctuation">[</span><span class="token punctuation">]</span>
+  outputFile<span class="token operator">:</span> <span class="token builtin">string</span>
+<span class="token punctuation">}</span><span class="token operator">&gt;</span>
+
+<span class="token keyword">const</span> defaultOptions <span class="token operator">=</span> <span class="token punctuation">{</span>
+  storeDir<span class="token operator">:</span> <span class="token string">&#39;src/store&#39;</span><span class="token punctuation">,</span>
+  excludes<span class="token operator">:</span> <span class="token punctuation">[</span><span class="token string">&#39;index&#39;</span><span class="token punctuation">]</span><span class="token punctuation">,</span>
+  outputFile<span class="token operator">:</span> <span class="token string">&#39;src/helper/pinia-auto-refs.ts&#39;</span><span class="token punctuation">,</span>
+<span class="token punctuation">}</span>
+</code></pre></div>`,13),e=[o];function c(i,u,l,r,k,d){return a(),s("div",null,e)}var f=n(p,[["render",c]]);export{g as __pageData,f as default};
